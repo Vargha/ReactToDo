c007367fb0ae7ef8b2dbbe4e061f75db83d7e906
Vargha <vhokmran@extensis.com>
2021-01-28 13:52:48 -0800

    Add todo form.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { hot } from 'react-hot-loader'
 import './App.css';
+import TodoList from "./todos/TodoList";
 
 const App = () => (
   <div className="App">
-    <h1>Hello React!</h1>
+    <h1>Todo List App</h1>
+    <TodoList/>
   </div>
 );
 
-export default App;
+export default hot(module)(App);
